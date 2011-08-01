0e9afbaec628b451b6a6cf5eecd82821eb5e19c2
Jose Diaz-Gonzalez <myaccounts@savant.be>
2011-08-01 03:01:01 -0400

    added some default server attributes

---
+++ sites/attributes/default.rb
@@ -0,0 +1,5 @@
+# Deploy settings
+default[:server][:production][:dir] = "/apps/production"
+default[:server][:production][:hostname] = "example.com"
+default[:server][:staging][:dir] = "/apps/staging"
+default[:server][:staging][:hostname] = "example.net"
