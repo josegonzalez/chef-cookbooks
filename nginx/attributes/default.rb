2d0bef50b7c6ae1ceeca694817d5de364818b27e
Jose Diaz-Gonzalez <myaccounts@savant.be>
2011-08-01 02:51:34 -0400

    Added nginx config from SG Cookbooks

---
+++ nginx/attributes/default.rb
@@ -0,0 +1,32 @@
+set[:nginx][:dir]     = "/etc/nginx"
+set[:nginx][:log_dir] = "/var/log/nginx"
+set[:nginx][:user]    = "www-data"
+set[:nginx][:binary]  = "/usr/sbin/nginx"
+
+set_unless[:nginx][:gzip] = "on"
+set_unless[:nginx][:gzip_http_version] = "1.0"
+set_unless[:nginx][:gzip_comp_level] = "2"
+set_unless[:nginx][:gzip_min_length] = "1024"
+set_unless[:nginx][:gzip_proxied] = "any"
+set_unless[:nginx][:gzip_static] = "off"
+set_unless[:nginx][:gzip_types] = [
+  "text/plain",
+  "text/css",
+  "application/x-javascript",
+  "application/json",
+  "text/xml",
+  "application/xml",
+  "application/xml+rss",
+  "text/javascript",
+  "image/png",
+  "image/gif",
+  "image/jpeg"
+]
+set_unless[:nginx][:gzip_vary] = "on"
+
+set_unless[:nginx][:keepalive]          = "on"
+set_unless[:nginx][:keepalive_timeout]  = 65
+set_unless[:nginx][:worker_processes]   = cpu[:total]
+set_unless[:nginx][:worker_connections] = 2048
+set_unless[:nginx][:server_names_hash_bucket_size] = 64
+set_unless[:nginx][:client_max_body_size] = "20M"
